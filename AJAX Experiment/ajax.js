65e400051a8a49505ec6947e116133d4fdbf9651
nickgordon1 <nicholasgordon990@gmail.com>
2016-03-05 12:59:44 -0600

    notes and query stuff

---
+++ AJAX Experiment/ajax.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){ 
+    $("div").load("target.txt");
+});
--- ToDoApp/app.js
+++ ToDoApp/app.js
@@ -1,28 +1,36 @@
-var listItems = [],
-    form = document.querySelector('.form'),
-    input = document.querySelector('.input'),
-    list = document.querySelector('.list');
-
-function renderItem(item) {
-  var li = document.createElement('li');
-  li.innerHTML = item;
-  list.appendChild(li);
-}
-
-function renderAll() {
-  list.innerHTML = '';
-  for (var i = 0; i < listItems.length; i++) {
-    renderItem(listItems[i]);
-  }
-}
-
-form.onsubmit = function() {  
-  listItems.push(input.value);
-  renderAll();
+$(document).ready(function(){
+                                 
+      var listItems = [];
   
-  input.value = '';
-  return false;
-};
-localStorage.setItem("item");
+      var storedItems = localStorage.getItem("olContent");
+  
+      if (storedItems) {
+      $('ol').html(storedItems);
+      }
 
-renderAll();
+      function renderAll() {
+      $('ol').html('');
+      
+      for (var i = 0; i < listItems.length; i++) {
+        $('ol').append("<li>" + listItems[i]);
+      }
+      
+      localStorage.setItem("olContent", $('ol').html() + "" );
+      }
+
+      $('.form').submit(function(e) {
+      e.preventDefault();
+      
+      listItems.push( $('input').val() );
+      renderAll();
+
+      $('.input').val('');
+      
+      $( "li" ).click(function() {
+      $( this ).fadeOut();
+            
+      });
+    
+    });
+
+});
